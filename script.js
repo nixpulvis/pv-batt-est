ccb9c36158b22484710d930e217efe6280b49c6d
Nathan Lilienthal <nathan@nixpulvis.com>
2024-12-18 14:08:33 -0500

    Finished parsing data (for now)

--- script.js
+++ script.js
@@ -12,22 +12,45 @@ fetch("http://localhost:8000/data.json")
 const INPUT_COLUMNS = [0,1,2]
 
 function populateSelectOptions() {
-    for (let i = 0; i < data[0].length; i++) {
-        console.log(`Adding options for ${data[0][i]}`)
+    let inputs = {};
+    for (let i = 0; i < INPUT_COLUMNS.length; i++) {
+        let key = data[0][INPUT_COLUMNS[i]];
+        console.log(`Adding options for ${key}`)
         // TODO: styling
         var label = $("<label>").prop("for", data[0][i]).text("  " + data[0][i]);
         var select = $("<select>").prop("id", data[0][i]);
 
-        // TODO: filter duplicates
-        for (let j = 1; j < data.length; j++) {
-            console.log(data[j][i]);
-            var option = $("<option>").val(data[j][i]).text(data[j][i]);
-            select.append(option);
+        for (let j = 0; j < data.length; j++) {
+            if (j == 0) {
+                inputs[key] = [];
+            } else {
+                var element = data[j][INPUT_COLUMNS[i]];
+                if (inputs[key].indexOf(element) === -1) {
+                    inputs[key].push(element);
+                    var option = $("<option>").val(element).text(element);
+                    select.append(option);
+                }
+            }
         }
 
         $("#input")
-            .append(select)
             .append(label)
+            .append(select)
             .append("<br>");
     }
+
+    // for (let i = 0; i < data[0].length; i++) {
+
+    //     // TODO: filter duplicates
+    //     for (let j = 1; j < data.length; j++) {
+    //         console.log(data[j][i]);
+    //         var option = $("<option>").val(data[j][i]).text(data[j][i]);
+    //         select.append(option);
+    //     }
+
+    //     $("#input")
+    //         .append(select)
+    //         .append(label)
+    //         .append("<br>");
+    // }
 }
