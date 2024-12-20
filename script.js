89de9d4ba504d59edeaccbbb3a6d54fedadbeffa
Nathan Lilienthal <nathan@nixpulvis.com>
2024-12-20 18:37:22 -0500

    Remove silly global value

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-var data;
-
 fetch("http://localhost:8000/data.json")
     .then(response => response.json())
-    .then(fetched => {
-        data = fetched;
-        populateSelectOptions();
+    .then(data => {
+        populateSelectOptions(data);
     })
     .catch(error => console.error("Error:", error));
 
-// TODO
+// TODO: Parse from metadata csv.
 const INPUT_COLUMNS = [0,1,2]
 
-function populateSelectOptions() {
+function populateSelectOptions(data) {
     let inputs = {};
     for (let i = 0; i < INPUT_COLUMNS.length; i++) {
         let key = data[0][INPUT_COLUMNS[i]];
-        console.log(`Adding options for ${key}`)
-        // TODO: styling
         var label = $("<label>").prop("for", data[0][i]).text("  " + data[0][i]);
         var select = $("<select>").prop("id", data[0][i]);
 
@@ -38,19 +33,4 @@ function populateSelectOptions() {
             .append(select)
             .append("<br>");
     }
-
-    // for (let i = 0; i < data[0].length; i++) {
-
-    //     // TODO: filter duplicates
-    //     for (let j = 1; j < data.length; j++) {
-    //         console.log(data[j][i]);
-    //         var option = $("<option>").val(data[j][i]).text(data[j][i]);
-    //         select.append(option);
-    //     }
-
-    //     $("#input")
-    //         .append(select)
-    //         .append(label)
-    //         .append("<br>");
-    // }
 }
