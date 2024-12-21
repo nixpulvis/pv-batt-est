a3dbf3bc3a70a9ce72dc39f7696d0b1838e9af26
Nathan Lilienthal <nathan@nixpulvis.com>
2024-12-20 19:11:08 -0500

    Add metadata file, next we'll use it.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-fetch("http://localhost:8000/data.json")
+fetch("http://localhost:8000/metadata.json")
+    .then(response => response.json())
+    .then(metadata => {
+        return fetch("http://localhost:8000/data.json");
+    })
     .then(response => response.json())
     .then(data => {
         populateSelectOptions(data);
